2fc868f66cf3d0fd18ddac34f77630c547018e30
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-03-31 22:31:18 -0700

    improved logic from pivotOn function and moved it to a command

--- src/main/java/frc/robot/Commands/RetractIntake.java
+++ src/main/java/frc/robot/Commands/RetractIntake.java
@@ -5,19 +5,24 @@
 package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.PivotConstants;
 import frc.robot.Subsystems.Intake;
+import frc.robot.Subsystems.Pivot;
 
 public class RetractIntake extends Command {
   /** Creates a new Intake. */
-  public Intake m_intake;
-  public double v_speedPercentagePivot;
+  private Intake m_intake;
+  private Pivot m_pivot;
+  private double m_speedPercentagePivot;
 
-  public RetractIntake(Intake intake, double speedPercentagePivot) {
+  public RetractIntake(Intake intake, Pivot pivot, double speedPercentagePivot) {
     m_intake = intake;
-    v_speedPercentagePivot = speedPercentagePivot;
+    m_pivot = pivot;
+    m_speedPercentagePivot = speedPercentagePivot;
 
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(intake);
+    addRequirements(intake, pivot);
   }
 
   // Called when the command is initially scheduled.
@@ -27,7 +32,24 @@ public class RetractIntake extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intake.pivotOn(v_speedPercentagePivot);
+    double currentPivotPosition = m_pivot.getPivotPosition();
+    boolean inRange = (currentPivotPosition > PivotConstants.upperPivotLimit) && (currentPivotPosition < PivotConstants.lowerPivotLimit);
+    boolean outsideLowRange = (currentPivotPosition <= PivotConstants.upperPivotLimit) && (m_speedPercentagePivot > 0); // RT (down)
+    boolean outsideHighRange = (currentPivotPosition >= PivotConstants.lowerPivotLimit) && (m_speedPercentagePivot < 0); // LT (up)
+
+    double halfIntakeSpeed = IntakeConstants.intakeSpeed/2;
+
+    if(inRange || outsideLowRange || outsideHighRange){
+      m_pivot.setPivotPercentage(m_speedPercentagePivot);
+
+      if(m_speedPercentagePivot <= 0){
+        m_intake.runRollers(halfIntakeSpeed);
+      }else{
+        m_intake.runRollers(-halfIntakeSpeed);
+      }
+    } else {
+      m_intake.stopMotors();
+    }
   }
 
   // Called once the command ends or is interrupted.
