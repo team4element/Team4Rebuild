6e9db8e1c08e0075fa3bf38f434498af9ed7f06c
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-03-31 22:42:27 -0700

    made a folder for commands used for auton

---
+++ src/main/java/frc/robot/Commands/Auton/IntakeForAuto.java
@@ -0,0 +1,20 @@
+/*
+ * This command is used for auto to bring the pivot of the intake down and intake the fuel. 
+ */
+
+package frc.robot.Commands.Auton;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.IntakeFuel;
+import frc.robot.Commands.PositionPivot;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Subsystems.Intake;
+import frc.robot.Subsystems.Pivot;
+
+public class IntakeForAuto extends SequentialCommandGroup {
+  /** Creates a new IntakeForAuto. */
+  public IntakeForAuto(Intake intake, Pivot pivot) {
+    // Add your commands in the addCommands() call, e.g.
+    addCommands(new PositionPivot(intake, pivot, 16).withTimeout(IntakeConstants.intakeTimeout), new IntakeFuel(intake, IntakeConstants.intakeSpeed).withTimeout(IntakeConstants.intakeTimeout));
+  }
+}
