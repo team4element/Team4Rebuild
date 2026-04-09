ff9e4ffc2f5091a92b3f4f35444f13418d21e1ad
Brandon <brandcomin@gmail.com>
2026-04-08 20:28:00 -0700

    Move and shoot works better while turning

--- src/main/java/frc/robot/Commands/ShootMove.java
+++ src/main/java/frc/robot/Commands/ShootMove.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.CommandSwerveDrivetrain;
 import frc.robot.Subsystems.Shooter;
 import frc.robot.Subsystems.Turret;
+import frc.robot.Constants.TurretConstants;
 import frc.robot.Constants.VisionConstants;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 
@@ -31,34 +32,48 @@ public class ShootMove extends Command {
         // Identify Target based on Alliance
         var alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
         int targetTagID = (alliance == Alliance.Blue) ? VisionConstants.centerHubBlueTag : VisionConstants.centerHubRedTag;
-        
+    
         var hubPose = m_fieldLayout.getTagPose(targetTagID);
-
         if (!hubPose.isEmpty()) {
             Translation2d hubCenterLocation = hubPose.get().toPose2d().getTranslation();
-            
+        
             // Get the compensated virtual target the turret is using for the "Lead"
             Translation2d virtualHubLocation = m_turret.calculateVirtualTarget(hubCenterLocation);
-
+        
             // Calculate distance from Turret to the Virtual Target for the regression
             double virtualDistanceMeters = virtualHubLocation.getDistance(m_turret.getTurretPose().getTranslation());
-
+        
             // Convert Robot-Relative Speeds to Field-Relative Speeds
             var state = m_drivetrain.getState();
             Rotation2d robotRotation = state.Pose.getRotation();
-            
-            double fieldVx = state.Speeds.vxMetersPerSecond * robotRotation.getCos() - state.Speeds.vyMetersPerSecond * robotRotation.getSin();
-            double fieldVy = state.Speeds.vxMetersPerSecond * robotRotation.getSin() + state.Speeds.vyMetersPerSecond * robotRotation.getCos();
-            Translation2d fieldVelocity = new Translation2d(fieldVx, fieldVy);
-
-            // Calculate Radial Velocity 
+            double fieldVx = state.Speeds.vxMetersPerSecond * robotRotation.getCos() 
+                           - state.Speeds.vyMetersPerSecond * robotRotation.getSin();
+            double fieldVy = state.Speeds.vxMetersPerSecond * robotRotation.getSin() 
+                           + state.Speeds.vyMetersPerSecond * robotRotation.getCos();
+        
+            // Tangential velocity from robot rotation (turret pivot traces an arc when spinning)
+            double omega = state.Speeds.omegaRadiansPerSecond;
+            Translation2d robotToTurret = new Translation2d(
+                TurretConstants.robotCenterToTurretForward,
+                -TurretConstants.robotCenterToTurretRight
+            );
+            double tangentialVx = omega * robotToTurret.getY();
+            double tangentialVy = -omega * robotToTurret.getX();
+            Translation2d tangentialVelocityField = new Translation2d(tangentialVx, tangentialVy)
+                                                    .rotateBy(robotRotation);
+        
+            double totalVx = fieldVx + tangentialVelocityField.getX();
+            double totalVy = fieldVy + tangentialVelocityField.getY();
+        
+            Translation2d fieldVelocity = new Translation2d(totalVx, totalVy);
+        
+            // Calculate Radial Velocity (project total velocity onto the turret-to-hub direction)
             Translation2d robotToHubVector = hubCenterLocation.minus(m_turret.getTurretPose().getTranslation());
             Rotation2d angleToHub = robotToHubVector.getAngle();
-            
-            // Project the field velocity onto the angle to the hub
-            double radialVelocityMps = fieldVelocity.getX() * angleToHub.getCos() + 
-                                       fieldVelocity.getY() * angleToHub.getSin();
-
+        
+            double radialVelocityMps = fieldVelocity.getX() * angleToHub.getCos() 
+                                     + fieldVelocity.getY() * angleToHub.getSin();
+        
             // Calculate and Set Target RPS
             double targetRPS = m_shooter.shootingDistanceVirtualTarget(virtualDistanceMeters, radialVelocityMps);
             m_shooter.setRPS(targetRPS);
